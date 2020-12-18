e8a27487e3d6de83a9f0449492df135e4d70a1bb
Erfan Andesta <erfan.andesta@mindvalley.com>
2020-12-18 15:21:11 +0330

    [CHORE] Initialized project

---
+++ DinDinn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DinDinn
+//
+//  Created by erfan on 9/28/1399 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
